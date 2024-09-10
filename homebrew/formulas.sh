b6934f36448f63f8d55b0725094be91a8836da92
Dima Mayboroda <mayboroda.de@gmail.com>
2024-09-10 11:03:57 +0200

    Add gpg

--- homebrew/formulas.sh
+++ homebrew/formulas.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env zsh
 
-brew install bat
-brew install brew-cask-completion
+brew install gpg
+brew install gawk
 brew install cmake
 brew install fzf
-brew install gawk
 brew install gnu-sed
 brew install htop
+brew install bat
 brew install jq
 brew install kubectx
 brew install neovim
@@ -16,11 +16,11 @@ brew install rust
 brew install shellcheck
 brew install stow
 brew install telnet
-brew install terraform
 brew install tmux
 brew install watch
 brew install wget
 brew install yq
+brew install brew-cask-completion
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
 brew install asdf
