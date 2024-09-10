fd627d54dccbe1e8e76aed0a8dd0b7601260c803
Dima Mayboroda <mayboroda.de@gmail.com>
2024-09-10 10:44:07 +0200

    Add asdf

--- homebrew/cask.sh
+++ homebrew/cask.sh
@@ -1,4 +1,4 @@
-#!/bin/zsh
+#!/usr/bin/env zsh
 
 brew install --cask wezterm@nightly
 brew install --cask nikitabobko/tap/aerospace
---
+++ homebrew/formulas.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env zsh
+
+brew install bat
+brew install brew-cask-completion
+brew install cmake
+brew install fzf
+brew install gawk
+brew install gnu-sed
+brew install htop
+brew install jq
+brew install kubectx
+brew install neovim
+brew install nmap
+brew install ripgrep
+brew install rust
+brew install shellcheck
+brew install stow
+brew install telnet
+brew install terraform
+brew install tmux
+brew install watch
+brew install wget
+brew install yq
+brew install zsh-autosuggestions
+brew install zsh-syntax-highlighting
+brew install asdf
+
