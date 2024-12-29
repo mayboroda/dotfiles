16e486e0a157416d9254499ae36be245fc19edc0
Dima Mayboroda <mayboroda.de@gmail.com>
2024-12-29 23:06:52 +0100

    Disable all unused spaces. WezTerm - add option to use Alt with Arrow keys. Emacs - removed

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -20,6 +20,12 @@ config = {
 		top = 5,
 		bottom = 5,
 	},
+  keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  }
 }
 
 return config
