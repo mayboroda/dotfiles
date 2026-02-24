06e85f238e084335cb6013615c9e4d3845d532aa
Dima Mayboroda <mayboroda.de@gmail.com>
2026-02-24 16:30:39 +0100

    Add empty home-manager

--- flake.nix
+++ flake.nix
@@ -5,14 +5,14 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      # Follow corresponding `release` branch from Home Manager
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs =
-    inputs@{
-      self,
-      nixpkgs,
-      nix-darwin,
-    }:
+  outputs = inputs@{self, nixpkgs, nix-darwin, home-manager, ...}:
     let
       system = "aarch64-darwin";
       hostname = "mayb";
@@ -22,6 +22,12 @@
         inherit system;
         modules = [
           ./darwin/configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            # home-manager.users.jdoe = ./home.nix;
+          }
         ];
       };
     };
