5198c32bec8ba794bbcf8c08dadd3fe07521d164
Dima Mayboroda <mayboroda.de@gmail.com>
2026-03-06 15:43:48 +0100

    move telescope to snackes

--- nvim/.config/nvim/lua/mayb/plugins/snacks.lua
+++ nvim/.config/nvim/lua/mayb/plugins/snacks.lua
@@ -2,6 +2,14 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
+  keys = {
+    { "<leader>tt", function() Snacks.picker() end, desc = "Snacks picker menu" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Snacks find files" },
+    { "<leader>fg", function() Snacks.picker.grep() end, desc = "Snacks live grep" },
+    { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Snacks buffers" },
+    { "<leader>fh", function() Snacks.picker.help() end, desc = "Snacks help tags" },
+    { "<leader>th", function() Snacks.picker.colorschemes() end, desc = "Select theme with preview" },
+  },
   opts = {
     picker = {
       enabled = true,
@@ -14,6 +22,6 @@ return {
         list  = { border = "none" },
         preview = { border = "none" },
       },
-    }
+    },
   }
 }
