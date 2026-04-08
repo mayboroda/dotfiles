8034b3df212d00d32c6506662830bd6bffbfa44e
Dima Mayboroda <mayboroda.de@gmail.com>
2026-04-08 17:43:26 +0200

    Add blink.cmp

--- nvim/.config/nvim_0_12/lua/plugins/lsp.lua
+++ nvim/.config/nvim_0_12/lua/plugins/lsp.lua
@@ -1,7 +1,25 @@
 vim.pack.add({
-  'https://github.com/nvim-mini/mini.indentscope'
+  {src='https://github.com/saghen/blink.cmp', version = 'v1'},
+  'https://github.com/rafamadriz/friendly-snippets',
+  'https://github.com/nvim-mini/mini.indentscope',
 })
 
 
 -- Not sure I need this one
 require('mini.indentscope').setup()
+
+
+-- In case of warning try to build `:BlinkCmp:build`
+require('blink.cmp').setup({
+  keymap = {
+    preset = 'default',
+  },
+  completion = {
+    list = { selection = { preselect = false, auto_insert = true } },
+  },
+  appearance = {
+    use_nvim_cmp_as_default = true,
+    nerd_font_variant = 'mono',
+  },
+  signature = { enabled = true },
+})
