5d333189e7ac38aff64d59e90a4342643d15f2f0
Dima Mayboroda <mayboroda.de@gmail.com>
2026-04-08 16:24:55 +0200

    Add mini.surround and mini.indentscope

---
+++ nvim/.config/nvim_0_12/lua/plugins/lsp.lua
@@ -0,0 +1,7 @@
+vim.pack.add({
+  'https://github.com/nvim-mini/mini.indentscope'
+})
+
+
+-- Not sure I need this one
+require('mini.indentscope').setup()
--- nvim/.config/nvim_0_12/lua/plugins/nvim.lua
+++ nvim/.config/nvim_0_12/lua/plugins/nvim.lua
@@ -1,8 +1,10 @@
 vim.pack.add({
   'https://github.com/nvim-treesitter/nvim-treesitter',
-  'https://github.com/tpope/vim-surround',
+  'https://github.com/nvim-mini/mini.surround',
 })
 
 vim.cmd.packadd('cfilter')
 vim.cmd.packadd('nvim.undotree')
 vim.cmd.packadd('nvim.difftool')
+
+require('mini.surround').setup()
